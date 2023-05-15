1f9bf826837494b8d769a52f946305dcd3fc4239
Morari Cosmin <cosminmorari99@yahoo.com>
2023-05-15 09:18:24 +0300

    script

--- scriptSinglePlayer.js
+++ scriptSinglePlayer.js
@@ -106,7 +106,7 @@ async function computerMove() {
       playerX.classList.add('currentPlayer');
       currentPlayer = gamer;
       if (cell != null) {
-        await wait(100); // Așteaptă 1 secundă
+        await wait(300); // Așteaptă 1 secundă
         computerTour();
       }
     }
